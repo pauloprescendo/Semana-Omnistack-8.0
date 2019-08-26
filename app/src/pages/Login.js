5cfe81bd53b6e710fc19808d62d4469ac7ae93e8
Paulo Prescendo <paulovinicius1994@hotmail.com>
2019-08-25 22:46:44 -0300

    main page app done

--- app/src/pages/Login.js
+++ app/src/pages/Login.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import  { Text, KeyboardAvoidingView, Platform, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import logo from '../assets/logo.png';
 
-export default function Login() {
+import api from '../services/api';
+
+export default function Login({ navigation }) {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then( user => {
+      if (user) {
+        navigation.navigate('Main', { user });
+      }
+    });
+  }, []);
+
+  async function handleLogin() {
+    const response = await api.post('/devs', { username: user });
+    const { _id } = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+
+    navigation.navigate('Main', { user: _id });
+  }
+
   return (
     <KeyboardAvoidingView
       style={styles.container}
@@ -17,9 +39,11 @@ export default function Login() {
         style={styles.input}
         placeholder="Digite seu uruário do Github"
         placeholderTextColor="#999"
+        value={user}
+        onChangeText={setUser}
       />
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity onPress={handleLogin} style={styles.button}>
         <Text style={styles.buttonText}>Enviar</Text>
       </TouchableOpacity>
     </KeyboardAvoidingView>
